b84d76d34128588c641ac83b857be387d220eb9a
“felipelm” <felipelm123@gmail.com>
2017-03-06 19:10:09 -0300

    adding name uniqueness on contact and custom_field

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ActiveRecord::Base
   has_many :custom_field_values
+  validates_uniqueness_of :name
 end
--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -1,2 +1,3 @@
 class CustomField < ActiveRecord::Base
+  validates_uniqueness_of :name
 end
