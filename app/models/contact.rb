9211e443f604cab6d72668a07251ed75cd377ac6
“felipelm” <felipelm123@gmail.com>
2017-03-07 16:12:23 -0300

    testing presence and uniqueness for contact email and custom_field name

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,4 @@
 class Contact < ActiveRecord::Base
   has_many :custom_field_values, dependent: :destroy
-  validates_uniqueness_of :email
+  validates :email, presence: true, uniqueness: true
 end
--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -1,3 +1,3 @@
 class CustomField < ActiveRecord::Base
-  validates_uniqueness_of :name
+  validates :name, presence: true, uniqueness: true
 end
